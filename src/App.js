8ca45e0e9ef97d2847c9c33f6886c4b206e68e2e
nao <nao_h13@hotmail.com>
2023-01-01 01:38:23 +0900

    reduxのTODOリスト実装

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./App.css";
+import { useSelector,useDispatch } from "react-redux";
 
 function App() {
+  const lists = useSelector((state) => state.lists);
+  const dispatch = useDispatch()
+  const doneList = (name) => {
+    dispatch({ type: "DONE_LIST", payload: name})
+  }
+  const deleteList = (name) => {
+    dispatch({ type: "DELETE_LIST", payload: name})
+  }
+
+  const [name, setName] = useState("")
+  const[complete,setComplete] = useState(false)
+
+  const inputText = (e) => {
+    setName(e.target.value);
+  }
+
+  const addList = () => {
+    if (!name) return;
+
+    setComplete(false);
+
+    dispatch({
+      type: "ADD_LIST",
+      payload: {
+        name,
+        complete
+      }
+    })
+    setName("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todoリスト</h1>
+      <input type="text" value={name} onChange={inputText}/>
+      <button onClick={addList}>追加</button>
+      <h2>未完了</h2>
+      <ul>
+        {lists
+          .filter((list) => list.complete === false)
+          .map((list, index) => (
+            <div key={index}>
+              {list.name}
+              <button onClick={() => doneList(list.name)}>完了</button>
+              <button onClick={() => deleteList(list.name)}>削除</button>
+            </div>
+          ))}
+      </ul>
+      <h2>完了済み</h2>
+      <ul>
+        {lists
+          .filter((list) => list.complete === true)
+          .map((list, index) => (
+            <div key={index}>
+              {list.name}
+              <button onClick={() => deleteList(list.name)}>削除</button>
+            </div>
+          ))}
+      </ul>
     </div>
   );
 }
